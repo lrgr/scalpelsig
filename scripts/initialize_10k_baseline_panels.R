8d94a67d5681a6e033f5f94261d4ecf1a38afc63
Nick Franzese <nick.franzese@gmail.com>
2020-07-06 17:34:54 -0400

    added number of windows in panel as a piece of information in the file name for baseline panels.

--- scripts/initialize_10k_baseline_panels.R
+++ scripts/initialize_10k_baseline_panels.R
@@ -5,7 +5,7 @@ source('projection_score.R')
 file_tag = "bp_test"
 
 NUM_PANELS = 1000
-NUM_WIN_IN_PANEL = 270
+NUM_WIN_IN_PANEL = 250
 
 print(paste0(Sys.time(), "    finding active 10kb windows."))
 possible_windows = active_10k_windows()
@@ -15,7 +15,7 @@ outprefix = paste0(GLOBAL_SCRIPT_BASELINE_WINDOWS, "baseline_panel_windows_", fi
 
 print(paste0(Sys.time(), "    sampling ", NUM_PANELS, " random panels with ", NUM_WIN_IN_PANEL, " windows each."))
 for (i in 1:NUM_PANELS) {
-	curr_outfile = paste0(outprefix, "_it", i, ".txt")
+	curr_outfile = paste0(outprefix, "_it", i, "_nwin", NUM_WIN_IN_PANEL, ".txt")
 	selected_windows = get_random_windows(NUM_WIN_IN_PANEL, possible_windows, check_overlap=FALSE)
 	write(selected_windows, curr_outfile)
 }
